8f4390eb13b24ec3b0e9a5240cd69cbe46a70abe
Animesh <animeshdeas@gmail.com>
2023-10-10 17:09:51 +0530

    Added mongoDB server , Added Sockets and make up connections

--- my-app/src/Components/textEditor.js
+++ my-app/src/Components/textEditor.js
@@ -1,8 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState,useRef,useCallback } from "react";
 import  {useParams} from "react-router-dom";
-import ReactQuill from 'react-quill';
+
 import { io } from "socket.io-client";
 import Quill from "quill";
+import "quill/dist/quill.snow.css"
 
 import 'react-quill/dist/quill.snow.css';
 
@@ -38,13 +39,57 @@ export default function TextEditor() {
       }
      
    },[]);
-  const modules = {
-    toolbar :TOOLBAR_OPTIONS,
-  };
+
+   useEffect(()=>{
+      
+     
+     
+   },[socket,quill])
+
+   useEffect(()=>{
+          
+       if(socket== null||quill==null) return ;
+       
+       socket.once('load-document',(document) =>{
+         quill.setContents(document);
+         quill.enable();
+
+       })
+       socket.emit("get-document",documentID);
+       
+     
+   },[socket,documentID,quill]);
+
+
+
+
+  const wrapperRef = useCallback((wrapper)=>{
+      
+    if(wrapper== null) return ;
+
+    wrapper.innerHTML = "";
+    const editor =document.createElement('div');
+    wrapper.append(editor);
+
+    const q = new Quill(editor,{
+         
+      theme : 'snow',
+      modules:{toolbar :TOOLBAR_OPTIONS}
+    })
+    
+    q.disable()
+    q.setText("Loading....");
+    setQuill(q);
+     
+  },[]);
     
   return (
- 
-    <ReactQuill theme="snow" modules={modules} />
+      
+    <div className="container" ref = {wrapperRef}>
+
+    </div>
+
+    
     
   );
 }
--- server/server.js
+++ server/server.js
@@ -1,4 +1,18 @@
 
+const mongoose  =require('mongoose');
+
+const Document  =require('./document');
+
+const defaultValue = "";
+
+mongoose.connect("mongodb://localhost:27017/GoogleDocument",{
+    useNewUrlParser: true, 
+    useUnifiedTopology:true,
+     
+
+});
+var connection   = mongoose.connection;
+ 
 
 const io =require('socket.io')(3001,{
     cors:{
@@ -10,7 +24,31 @@ const io =require('socket.io')(3001,{
 io.on('connection',(socket)=>{
      console.log(socket+"Connected");
 
+
+    socket.on('get-document',async documentID =>{
+              
+          const document  =await findOrCreateDocument(documentID);
+          socket.join(documentID);
+          socek.emit('load-document',document.data)
+    });
+      
+
      socket.on('disconnect',()=>{
         console.log("Client Disconneted");
      })
-})
+
+
+});
+
+async function findOrCreateDocument(id)
+{
+      if(id==null) return ;
+
+      const document = await Document.findByID(id);
+      if(document) return document;
+
+      return await Document.create({
+         _id: id,
+         data :defaultValue,
+      })
+}
